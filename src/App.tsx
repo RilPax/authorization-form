526fdefacb60e3acbb7b0e6d65c90d94c473c684
Нормуминов Руслан <RilPax@yandex.ru>
2025-08-27 20:17:48 +0500

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Tailwind 4 🎉 работает!</h1>
+    </div>
+  )
+}
+
+export default App

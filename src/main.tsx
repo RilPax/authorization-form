f12a527e94fb8b73106653266f3f7ca88f0622e8
Нормуминов Руслан <RilPax@yandex.ru>
2025-08-27 22:43:20 +0500

    created routes/store template, userSlice template

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter,  } from "react-router-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./components/app/App.tsx";
+import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
---
+++ src/store/slices/user.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface userSliceState {
+
+}
+
+const initialState: userSliceState = {
+
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => (
+        builder
+            
+    )
+
+})
+
+export default userSlice.reducer
